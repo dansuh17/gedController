704a1c264c11907071c8e04a4a3e4a1493a638a6
Park Junwoo <junwoopark0130@gmail.com>
2016-07-20 12:36:20 -0400

    implemented timer.

--- public/app/components/main/mainController.js
+++ public/app/components/main/mainController.js
@@ -2,4 +2,6 @@ app.controller('MainCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav)
         $scope.toggleSidenav = function(menuId) {
                 $mdSidenav(menuId).toggle();
         };
+
+        $scope.timerRunning = true;
 }]);
