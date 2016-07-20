83a0118bd01d9c5494b82fd76e0a87cf6d2bcb86
Park Junwoo <junwoopark0130@gmail.com>
2016-07-20 09:42:16 -0400

    set up material design. added background-image

---
+++ public/app/components/main/mainController.js
@@ -0,0 +1,5 @@
+app.controller('MainCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+        $scope.toggleSidenav = function(menuId) {
+                $mdSidenav(menuId).toggle();
+        };
+}]);
