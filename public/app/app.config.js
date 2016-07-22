fc40cba9a38759cbd1e62248b525d34fe224626f
DongwooSuh <kehops@kaist.ac.kr>
2016-07-22 15:21:37 -0400

    total change in layouts and design

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,20 +1,4 @@
+/* Do we even need this file?? */
 app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default').primaryPalette('grey').dark();
-
-  /*
-    var customBlueMap = $mdThemingProvider.extendPalette('light-blue', {
-        'contrastDefaultColor': 'light',
-        'contrastDarkColors': ['50'],
-        '50': 'ffffff'
-    });
-    $mdThemingProvider.definePalette('customBlue', customBlueMap);
-    $mdThemingProvider.theme('default')
-    .primaryPalette('customBlue', {
-        'default': '500',
-        'hue-1': '50'
-    })
-    .accentPalette('pink');
-    $mdThemingProvider.theme('input', 'default')
-        .primaryPalette('grey')
-        */
 });
--- public/app/components/main/mainController.js
+++ public/app/components/main/mainController.js
@@ -8,98 +8,21 @@ app.controller('MainCtrl', ['$scope', '$mdSidenav', 'socket', function($scope, $
     name: "Devin Powell"
   };
 
-  $scope.powerBalance = 40;
+  $scope.powerBalance = 22;
 
   /**
-   * Call the appropriate function according to input arguments
-   * @param fighter fighter's name - either 'tom' or 'devin'
-   * @param part the part where damage was applied - either 'head' or 'body'
-   * @param amount the amount of damage
+   * Sets the power balance. If power balance is under 50, it indicates
+   * that the right side has dominance over left, and vice versa.
+   * @param powerBalance power balance amount. 0 ~ 99
    */
-  $scope.energyChange = function(fighter, part, amount) {
-    if (fighter == 'tom') {
-      if (part == 'head') {
-        $scope.$apply($scope.tomHeadEnergy(amount));
-      } else {
-        $scope.$apply($scope.tomBodyEnergy(amount));
-      }
-    } else {
-      if (part == 'head') {
-        $scope.$apply($scope.devinHeadEnergy(amount));
-      } else {
-        $scope.$apply($scope.devinBodyEnergy(amount));
-      }
-    }
+  $scope.setPowerBalance = function(powerBalance) {
+    $scope.powerBalance = powerBalance;
   };
 
-  socket.on('energyBarChange', function(data) {
-    console.log("energyBar received.");
-    //apply energybar accordingly.
-    $scope.energyChange(data.fighter, data.part, parseInt(data.amount, 10));
+  socket.on('setPowerBalance', function(data) {
+    console.log('setPowerBalance received.' + data.balance);
+    $scope.apply(function () {
+      $scope.setPowerBalance(parseInt(data.balance, 10));
+    });
   });
-
-
-  /**
-   * change tom's body energy by amount
-   * @param amount
-   */
-  $scope.tomBodyEnergy = function(amount) {
-    $scope.tom.bodyenergy += amount;
-
-    if ($scope.tom.bodyenergy >= 80) {
-      $scope.tom.bodyEnergyStatus = "progress-bar-success";
-    } else if ($scope.tom.bodyenergy >= 40) {
-      $scope.tom.bodyEnergyStatus = "progress-bar-warning";
-    } else {
-      $scope.tom.bodyEnergyStatus = "progress-bar-danger";
-    }
-  };
-
-  /**
-   * change devin's body energy by AMOUNT
-   * @param amount the damage amount
-   */
-  $scope.devinBodyEnergy = function(amount) {
-    $scope.devin.bodyenergy += amount;
-
-    if ($scope.devin.bodyenergy >= 80) {
-      $scope.devin.bodyEnergyStatus = "progress-bar-success";
-    } else if ($scope.devin.bodyenergy >= 40) {
-      $scope.devin.bodyEnergyStatus = "progress-bar-warning";
-    } else {
-      $scope.devin.bodyEnergyStatus = "progress-bar-danger";
-    }
-  };
-
-  /**
-   * change tom's head energy by AMOUNT
-   * @param amount
-   */
-  $scope.tomHeadEnergy = function(amount) {
-    $scope.tom.headenergy += amount;
-
-    if ($scope.tom.headenergy >= 80) {
-      $scope.tom.headEnergyStatus = "progress-bar-success";
-    } else if ($scope.tom.headenergy  >= 40) {
-      $scope.tom.headEnergyStatus = "progress-bar-warning";
-    } else {
-      $scope.tom.headEnergyStatus = "progress-bar-danger";
-    }
-  };
-
-  /**
-   * change devin's head energy by AMOUNT
-   * @param amount
-   */
-  $scope.devinHeadEnergy = function(amount) {
-    $scope.devin.headenergy += amount;
-
-    if ($scope.devin.headenergy >= 80) {
-      $scope.devin.headEnergyStatus = "progress-bar-success";
-    } else if ($scope.devin.headenergy  >= 40) {
-      $scope.devin.headEnergyStatus = "progress-bar-warning";
-    } else {
-      $scope.devin.headEnergyStatus = "progress-bar-danger";
-    }
-  };
 }]);
