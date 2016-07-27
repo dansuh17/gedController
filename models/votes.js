15407e6cf41192bdc3fd58598c82a73b0e7d2362
Park Junwoo <junwoopark0130@gmail.com>
2016-07-27 13:27:42 -0400

    hotfix on mongoose schema.

--- models/votes.js
+++ models/votes.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var VoteSchema = new mongoose.Schema({
-    tomUp: Integer, 
-    devinUp: Integer,
+    tomUp: Number, 
+    devinUp: Number,
 },
 {
     timestamps: true
