8f7690aaec8e1294293e7b7bccb5d979870266d9
DongwooSuh <kehops@kaist.ac.kr>
2016-07-27 15:43:41 -0400

    tap ver2 + socket callback functions for tapping

--- public/interaction/tap2.js
+++ public/interaction/tap2.js
@@ -19,26 +19,22 @@
 
     /* adjust css properties of tap A region */
     $('#tapA')
-        .css('display', 'flex')
-        .css('align-items', 'center')
         .css('position', 'fixed')
-        .css('top', 0.3 * windowHeight)
+        .css('bottom', '20px')
         .css('left', 0.1 * windowWidth)
-        .css('height', 0.6 * windowHeight)
+        .css('height', 0.2 * windowHeight)
         .css('width', 0.3 * windowWidth);
 
     /* adjust css properties of tap B region */
     $('#tapB')
-        .css('display', 'flex')
-        .css('align-items', 'center')
         .css('position', 'fixed')
-        .css('top', 0.3 * windowHeight)
+        .css('bottom', '20px')
         .css('right', 0.3 * windowHeight)
-        .css('height', 0.6 * windowHeight)
+        .css('height', 0.2 * windowHeight)
         .css('width', 0.3 * windowWidth);
 
     /* 'onclick' functions for tapping regions -
-       works for both mouse and touch (if on mobile) */
+     works for both mouse and touch (if on mobile) */
     $("#tapA").bind('mousedown touchstart', function () {
       console.log('tapA clicked');
       socket.emit('addPowerBalance', {amount: -2});
