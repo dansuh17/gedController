4f7c690c06a28a191dd99fcdc9cddb32ca2647fc
Park Junwoo <junwoopark0130@gmail.com>
2016-07-28 10:56:11 -0400

    favicon setup

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
