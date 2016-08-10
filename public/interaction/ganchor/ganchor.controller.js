7e46e7bb99bd35ff4b2405c474c6129a9ed1e6f7
minki chung <brekkanegg@gmail.com>
2016-08-10 15:11:41 -0400

    graph toggle button

--- public/interaction/ganchor/ganchor.controller.js
+++ public/interaction/ganchor/ganchor.controller.js
@@ -1,10 +1,20 @@
 /**
- * Created by deNsuh on 8/9/16.
+ *
+ *
+ *
+ * this is for the graph anchor in order to show up the graph only when the button is clicked
+ * this uses graph in d3
+ *
+ *
+ * Created by Minki Chung on 8/9/16.
  */
 ;(function() {
   angular
       .module('ganchor')
       .controller('ganchorCtrl', ['$scope', function($scope) {
-        $scope.anchor_button = "../path/to/image.png";
+          //scope.anchor_button = "../path/to/image.png";
+          //$scope.graph = "http://www.w3schools.com/angular/ng_ng-src.asp"
+          $scope.graphOn = false;
+
       }]);
 })();
--- public/interaction/ganchor/ganchor.module.js
+++ public/interaction/ganchor/ganchor.module.js
@@ -2,7 +2,12 @@
  * graph anchor module.
  * by Minki Chung
  */
+
 ;(function() {
   angular
       .module('ganchor', [])
 })();
+
+//var ganchor = angular.module("ganchor", []);
+
+
