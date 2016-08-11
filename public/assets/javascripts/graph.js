31b5beb861e6b8855bb5fdd7cfd9ebf7ec8d653e
minki chung <brekkanegg@gmail.com>
2016-08-11 11:00:15 -0400

    ?

--- public/assets/javascripts/graph.js
+++ public/assets/javascripts/graph.js
@@ -6,6 +6,9 @@ var fighterA = "http://www.sherdog.com/image_crop.php?image=http://www.origin.sh
 var fighterB = "http://www.sherdog.com/image_crop.php?image=http://www.origin.sherdog.com/_images/fighter/20160619102650_DevinPowell.JPG&&width=200&&height=300";
 var eMark = "assets/images/1470249409_interface-40.svg";
 
+var timezone = -(1000*60*60*4) + (1000*60*20.7); //- jsTime
+var jsTime = 0;
+
 var currentTick = 0;
 var svg = d3.select("svg"),
 margin = {top: 100, right: 100, bottom: 100, left: 100};
@@ -171,16 +174,14 @@ function addMark() {
 }
 
 function addMarkAt(time_) {
-    var tick = (new Date(time_).getTime() - gameStartTime+1000*60*60*2) / 1000 + (currentRound-1)*300;
-    console.log(tick);
+    var tick = (new Date(time_).getTime() - eventStartTime) / 1000 + (currentRound - 1) * 300;
+    console.log("tick : "+tick);
 
-    if(tick > currentTick && tick < 0){
-        return;
-
-    if(tick < currentTick && tick > 0){
+    if (tick < currentTick && tick > 0) {
         tick = Math.floor(tick);
         var tempHighlight = JSON.parse(reqHighlight.response).comments.el;
-        var tempHighlightVideo
+        var tempHighlightVideo = 0;
+        done++;
         g.append("svg:image")
             .attr("class", "eMark")
             .attr("xlink:href", eMark)
@@ -189,142 +190,153 @@ function addMarkAt(time_) {
             .attr("x", width / n * (tick - 1) - 15)
             .attr("y", data[tick - 1] * (-1) * (height / 2) + (height / 2) - 15)
             ///tooltip
-            .on("mouseover", function(d) {
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div	.html(JSON.parse(reqHighlight.response).comments.el[done]) // done is variable for the order of highlights
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-            .on("mouseout", function(d) {
+            .on("mouseover", function (d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html(JSON.parse(reqHighlight.response).comments.el[done]) // done is variable for the order of highlights
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
                 div.transition()
                     .duration(500)
                     .style("opacity", 0);
             });
 
     }
-    tick = Math.floor(tick);
-    g.append("svg:image")
-        .attr("class", "eMark")
-        .attr("xlink:href", eMark)
-        .attr("width", 30)
-        .attr("height", 30)
-        .attr("x", width / n * (tick - 1) - 15)
-        .attr("y", data[tick - 1] * (-1) * (height / 2) + (height / 2) - 15)
+
 }
 
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJraXN3ZSIsInN1YiI6IjU3M2UxMzM4YTYzZjU5OWEwY2M4NjY1YyIsImV4cCI6IjIxMTUtMTEtMTZUMjA6MDg6MjkuNDg0WiJ9.L-JdjzIZ0Y6LHhtvygVyl-_DJUvJ7PWjbapNfp_Ea1s";
 
 
 ///event id
-var eventID = "wsof_testrun_20160804140831";
-var eventIS = "stage-api" //if it is on the prod, use "api-v4" instead
+    var eventID = "wsof_wsof32_20160727140717";
+    var eventIS =
+        //for prod use
+          "api-v4";
+        //for stage use
+        //"stage-api";
 
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJLaXN3ZSIsInN1YiI6IjU3M2UxMzU5NmU0ZjEzNjEwYWY5YjY4ZCIsImV4cCI6IjIwMTYtMTEtMTlUMTk6Mjk6NDYuMzE3WiJ9.AxazxY2ToE4e8qEOZEobI7jKbRf_P1xezJbps_8KrPI";
-//if using prod, use this token instead.
-//"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJraXN3ZSIsInN1YiI6IjU3M2UxMzM4YTYzZjU5OWEwY2M4NjY1YyIsImV4cCI6IjIxMTUtMTEtMTZUMjA6MDg6MjkuNDg0WiJ9.L-JdjzIZ0Y6LHhtvygVyl-_DJUvJ7PWjbapNfp_Ea1s"
-// >>>>>>> a7451ed6db8f3707131c212fe548145f8b7067a6
+    var token =
+        //for stage, use this token
+        //"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJLaXN3ZSIsInN1YiI6IjU3M2UxMzU5NmU0ZjEzNjEwYWY5YjY4ZCIsImV4cCI6IjIwMTYtMTEtMTlUMTk6Mjk6NDYuMzE3WiJ9.AxazxY2ToE4e8qEOZEobI7jKbRf_P1xezJbps_8KrPI";
+        //for prod, use this token
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJraXN3ZSIsInN1YiI6IjU3M2UxMzM4YTYzZjU5OWEwY2M4NjY1YyIsImV4cCI6IjIxMTUtMTEtMTZUMjA6MDg6MjkuNDg0WiJ9.L-JdjzIZ0Y6LHhtvygVyl-_DJUvJ7PWjbapNfp_Ea1s"
 
-var eventStartTime = new Date().getTime();
-var gameStartTime;
-var timediff = -14400000; // for eastern time -4;
+    var gameStartTime;
+    var eventStartTime = new Date().getTime() + timeZone;
+    var timediff = 0;
 
-var reqEvent = new XMLHttpRequest();
+    var reqEvent = new XMLHttpRequest();
 
-reqEvent.open('GET', 'https://' + eventIS +'.kiswe.com:443/api/events/id/' + eventID, true);
-reqEvent.setRequestHeader("Authorization", token);
-reqEvent.send(null);
+    reqEvent.open('GET', 'https://' + eventIS + '.kiswe.com:443/api/events/id/' + eventID, true);
+    reqEvent.setRequestHeader("Authorization", token);
+    reqEvent.send(null);
 
-var isStart = false;
-var currentRound = 0;
+    var isStart = false;
+    var currentRound = 0;
 
-var currentWinning = 0;
-var gameGoingOn = true; ////////////
+    var currentWinning = 0;
+    var gameGoingOn = true; ////////////
 
-var reqHighlight = new XMLHttpRequest();
-var clipData = new Object();
-var done = 0;
+    var reqHighlight = new XMLHttpRequest();
+    var clipData = new Object();
+    var done = 0;
 
-var flag = false;
-var flag2 = 0;
+    var flag = false;
+    var flag2 = 0;
 
-function tick() {
-    // Redraw the line.
-    if(currentTick <= n){
+    function tick() {
+        // Redraw the line.
+        if (currentTick <= n) {
 
-        $.ajax({
-            url: "http://ged.uwcj.kr:3000/votes/getCurrentWinning",
-            dataType: "jsonp",
-            success: function (data) {
-                currentWinning = ((data.devinUp / (data.tomUp + data.devinUp)) - 0.5) / 0.5;
+            $.ajax({
+                url: "http://ged.uwcj.kr:3000/votes/getCurrentWinning",
+                dataType: "jsonp",
+                success: function (data) {
+                    currentWinning = ((data.devinUp / (data.tomUp + data.devinUp)) - 0.5) / 0.5;
 
-                if (currentWinning > 0) {
-                    tomWinning = true;
-                    devinWinning = false;
-                } else if (currentWinning < 0) {
-                    tomWinning = false;
-                    devinWinning = true;
+                    if (currentWinning > 0) {
+                        tomWinning = true;
+                        devinWinning = false;
+                    } else if (currentWinning < 0) {
+                        tomWinning = false;
+                        devinWinning = true;
+                    }
+                    gameGoingOn = data.gameGoingOn;
                 }
-                gameGoingOn = data.gameGoingOn;
-            }
-        });
+            });
 
-        if(flag) {
-            if(flag2<1){
-                timediff += eventStartTime - (new Date(JSON.parse(reqEvent.response).event.start_time).getTime());
-                flag2++;
-                // console.log(flag2);
-            }
-            clipData = JSON.parse(reqHighlight.response).comments.el;
-            // timediff = (new Date().getTime()) - (new Date(clipData[0].start_time).getTime());
-            for(var i = done; i<clipData.length; i++) {
-                if((new Date(clipData[i].start_time).getTime()) + timediff < (new Date().getTime())) {
-                    addMarkAt(clipData[i].start_time);
-                    done++;
-                } else {
-                    break;
+            if (flag) {
+                if (flag2 < 1) {
+                    timediff += eventStartTime - ((new Date(JSON.parse(reqEvent.response).event.start_time).getTime()));
+                    // flag2++;
+                    // console.log(flag2);
+                    console.log("real event start in db : "+JSON.parse(reqEvent.response).event.start_time);
+                    console.log("real event start in date format : "+new Date(JSON.parse(reqEvent.response).event.start_time));
+                    console.log("current event play : "+new Date(eventStartTime));
+                    console.log("time diff in : "+timediff/1000/60/60/24 + " day + "+(timediff/1000/60/60)%24 + " hours");
                 }
+                if(reqHighlight.response.length > 10) {
+                    clipData = JSON.parse(reqHighlight.response).comments.el;
+                    if (flag2 < 1) {
+                        console.log("first clip time in db : " + clipData[0].start_time);
+                        console.log("first clip time in date format : " + new Date(clipData[0].start_time));
+                        console.log("first clip in min : "+((new Date(clipData[0].start_time).getTime()) + timediff-eventStartTime)/1000/60);
+                    }
+                    flag2++;
+                    // timediff = (new Date().getTime()) - (new Date(clipData[0].start_time).getTime());
+                    for (var i = done; i < clipData.length; i++) {
+                        var startIn = (new Date(clipData[i].start_time).getTime()) + timediff - (new Date().getTime() + timezone);
+                        console.log(startIn);
+                        if (startIn < 0) {
+                            addMarkAt(clipData[i].start_time);
+                            // done++;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                // console.log(clipData);
             }
-            // console.log(clipData);
-        }
-        flag = true;
-          reqHighlight.open('GET', 'https://' + eventIS +'.kiswe.com:443/api/comments/' + eventID, true);
-        reqHighlight.setRequestHeader("Authorization", token);
-        reqHighlight.send(null);
+            flag = true;
+            reqHighlight.open('GET', 'https://' + eventIS + '.kiswe.com:443/api/comments/' + eventID, true);
+            reqHighlight.setRequestHeader("Authorization", token);
+            reqHighlight.send(null);
 
-        if (gameGoingOn) {
-            if(!isStart){
-                currentRound++;
-                gameStartTime = new Date().getTime();
+            if (gameGoingOn) {
+                if (!isStart) {
+                    currentRound++;
+                    gameStartTime = new Date().getTime() + timezone;
+                }
+                isStart = true;
+                data.push(currentWinning);
+                currentTick++;
+                // if (Math.random() > 0.8) {
+                //     addMarkAt();
+                // }
+            } else {
+                isStart = false;
             }
-            isStart = true;
-            data.push(currentWinning);
-            currentTick++;
-             if(Math.random()>0.8){
-             	addMark();
-             }
-        } else {
-            isStart = false;
+
+            d3.active(this)
+                .transition()
+                .on("start", tick);
+            if (currentWinning >= 0) {
+                d3.select(this)
+                    .attr("d", d3line)
+                    .attr("transform", null)
+                    .style("fill", "none")
+                    .style("stroke", "e91a67");
+            }
+            else if (currentWinning < 0) {
+                d3.select(this)
+                    .attr("d", d3line)
+                    .attr("transform", null)
+                    .style("fill", "none")
+                    .style("stroke", "32bdf0");
+            }
         }
 
-        d3.active(this)
-            .transition()
-            .on("start", tick);
-        if (currentWinning >= 0) {
-            d3.select(this)
-                .attr("d", d3line)
-                .attr("transform", null)
-                .style("fill", "none")
-                .style("stroke", "e91a67");
-        }
-        else if (currentWinning < 0) {
-            d3.select(this)
-                .attr("d", d3line)
-                .attr("transform", null)
-                .style("fill", "none")
-                .style("stroke", "32bdf0");
-        }
-    }
 }
-
