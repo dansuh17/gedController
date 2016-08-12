a6a0c6b67791af0030ce3d70732655b777797fc4
DongwooSuh <kehops@kaist.ac.kr>
2016-08-12 10:29:29 -0400

    Twitter feed box fix and some cleanup

--- socket.js
+++ socket.js
@@ -13,7 +13,7 @@ var T = new Twit({
   consumer_key:         keys.consumer_key,
   consumer_secret:      keys.consumer_secret,
   access_token:         keys.access_token,
-  access_token_secret:  keys.access_token_secret,
+  access_token_secret:  keys.access_token_secret
 });
 
 var index = {
