89c734081c8b10b28a4431014ccf2634169247c2
DongwooSuh <kehops@kaist.ac.kr>
2016-08-16 01:43:49 -0400

    Added punch image to tap on and increase punch count on 'tap'

--- public/interaction/tap/tap.controller.js
+++ public/interaction/tap/tap.controller.js
@@ -7,7 +7,23 @@
       .module('tap')
       .controller('tapCtrl', ['$scope', 'socketFactory', function ($scope, socketFactory) {
         // cute kiswe logo
-        $scope.logoUrl = '../assets/images/kisweLogo.png';
+        $scope.logoUrl = '../../assets/images/kisweLogo.png';
+        $scope.punchImage1 = '../../assets/images/punch_grey.png'; // left
+        $scope.punchImage2 = '../../assets/images/punch_red.png'; // right
+        $scope.fighterCount1 = 0;
+        $scope.fighterCount2 = 0;
+
+        /**
+         * Increment per tap the punch count for each fighter.
+         * @param num the fighter number.
+         */
+        $scope.increaseFighterPunchCount = function(num) {
+          if (num == 1) {
+            $scope.fighterCount1++;
+          } else if (num == 2) {
+            $scope.fighterCount2++;
+          }
+        };
 
         /**
          * Moves the balance bar by AMOUNT. This calls the 'addPowerBalance' function
