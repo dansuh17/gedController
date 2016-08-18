7e28a563790bd3a64feabd7e21f899ae90947c60
DongwooSuh <kehops@kaist.ac.kr>
2016-08-18 13:26:04 -0400

    completed minor linting

--- public/app/components/ballmove/p5.particlemove.js
+++ public/app/components/ballmove/p5.particlemove.js
@@ -2,7 +2,7 @@
  * Ball move particle physical engine design for quizquiz modeling.
  * by Young Jin Park
  */
-
+/* global $:true, width:true, height:true */
 var particles;
 var suns = []; // Center of Gravity
 var totalcount = 20;
@@ -47,12 +47,12 @@ function setup() {
   for (i = 0; i < totalcount; i++) {
     // set the initial position of particle and create particle
     particle = createSprite(width / 2, height / 2, 15, 15);
-    n = floor(random(0, 6)); // randomize the faces to draw
-    particle.setSpeed(random(0, 80), random(0, 50));
+    n = Math.floor(Math.random(0, 6)); // randomize the faces to draw
+    particle.setSpeed(Math.random(0, 80), Math.random(0, 50));
     particle.addAnimation('normal', faces[n]);
 
     // assign the pole randomly to each particle
-    if (random(0, 1) < 0.5) {
+    if (Math.random(0, 1) < 0.5) {
       particle.pole = 0;
       curleftcount += 1;
     } else {
@@ -67,28 +67,30 @@ function setup() {
 
 function draw() {
   var promise;
+  var i;
+  var s;
 
   background(255, 255, 255, 0);
   clear();
 
   // add text
   textSize(60);
-  fill(24,24,24);
+  fill(24, 24, 24);
   strokeWeight(4);
-  stroke(207,207,207);
+  stroke(207, 207, 207);
   textFont(helveticanueBold);
   text((leftcount * 5) + '%', 110, suns[0].position.y);
   text((rightcount * 5) + '%', 850, suns[1].position.y);
 
   textSize(55);
   noStroke();
-  fill(119,119,119);
+  fill(119, 119, 119);
   textFont(helveticanue);
   textAlign(CENTER);
-  text('WHO WILL WIN?', width / 2 + 25, height / 2 - 130);
+  text('WHO WILL WIN?', (width / 2) + 25, (height / 2) - 130);
 
   textSize(40);
-  text('VOTE NOW', width / 2 + 25, height / 2 - 90);
+  text('VOTE NOW', (width / 2) + 25, (height / 2) - 90);
 
   // take the value from ajax call
   promise = getData();
@@ -129,8 +131,8 @@ function draw() {
   }
 
   // update all particles
-  for (var i = 0; i < particles.length; i++) {
-    var s = particles[i];
+  for (i = 0; i < particles.length; i++) {
+    s = particles[i];
 
     // update particle speed
     updateParticle(s);
@@ -139,22 +141,22 @@ function draw() {
     var wallmargin = 35;
     if (s.position.x < wallmargin) {
       s.position.x = wallmargin + 1;
-      s.velocity.x = abs(s.velocity.x);
+      s.velocity.x = Math.abs(s.velocity.x);
     }
 
     if (s.position.x > width - wallmargin) {
       s.position.x = width - wallmargin - 1;
-      s.velocity.x = -abs(s.velocity.x);
+      s.velocity.x = -Math.abs(s.velocity.x);
     }
 
     if (s.position.y < wallmargin) {
       s.position.y = wallmargin + 1;
-      s.velocity.y = abs(s.velocity.y);
+      s.velocity.y = Math.abs(s.velocity.y);
     }
 
     if (s.position.y > height - wallmargin) {
       s.position.y = height - wallmargin - 1;
-      s.velocity.y = -abs(s.velocity.y);
+      s.velocity.y = -Math.abs(s.velocity.y);
     }
   }
 
@@ -170,8 +172,8 @@ function updateParticle(p) {
   var rx = suns[p.pole].position.x - p.position.x;
   var ry = suns[p.pole].position.y - p.position.y;
 
-  var ax = (0.002 * rx) - (0.1 * p.velocity.x) + random(-2, 2);
-  var ay = (0.002 * ry) - (0.1 * p.velocity.y) + random(-2, 2);
+  var ax = (0.002 * rx) - (0.1 * p.velocity.x) + Math.random(-2, 2);
+  var ay = (0.002 * ry) - (0.1 * p.velocity.y) + Math.random(-2, 2);
 
   p.velocity.x += ax;
   p.velocity.y += ay;
